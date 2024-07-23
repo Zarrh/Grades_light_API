7e60d7e28cb1890363f531394eedbff38c7f29be
Zarrh <fagaraz.luca@gmail.com>
2024-07-23 20:15:04 +0200

    Added authentication on upload and data retrieve

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask, request, Response
+from flask import Flask, request, Response, jsonify
 import os
 from Crypto.Hash import SHA3_256
+import json
 
 app = Flask(__name__)
 
@@ -20,19 +21,30 @@ def retrieve():
     request_key = request.form['api_key']
     hashed_key = SHA3_256.new(data=request_key.encode("utf-8"))
     if hashed_key.hexdigest() == key:
-        return Response("Valid key", status=200)
+        try:
+            with open("data/grades.json", 'r') as file:
+                grades_data = json.load(file)
+            return Response(jsonify(grades_data), status=200)
+        except FileNotFoundError:
+            return Response(jsonify({'error': 'File not found'}), status=404)
+        except json.JSONDecodeError:
+            return Response(jsonify({'error': 'Error decoding JSON'}), status=500)
     return Response("Invalid API key", status=401)
 
 
 @app.route('/update', methods=['POST'])
 def update():
-    if 'file' not in request.files:
-        return Response("File not found", status=400)
+    if 'file' not in request.files or not request.form['api_key']:
+        return Response("Bad request", status=400)
+    request_key = request.form['api_key']
+    hashed_key = SHA3_256.new(data=request_key.encode("utf-8"))
     file = request.files['file']
-    if file.filename == 'grades.json':
-        file.save(os.path.join("data", file.filename))
-        return Response("File saved successfully", status=200)
-    return Response("Bad request", status=400)
+    if hashed_key.hexdigest() == key:
+        if file.filename == 'grades.json':
+            file.save(os.path.join("data", file.filename))
+            return Response("File saved successfully", status=200)
+        return Response("Bad request", status=400)
+    return Response("Invalid API key", status=401)
 
 
 if __name__ == '__main__':
